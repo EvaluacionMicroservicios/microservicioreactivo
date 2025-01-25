7792914464882cb285340d520b4f0d2dc7327e74
Ronnie Gonzales <ronnie_cepafs@hotmail.com>
2025-01-25 04:57:36 -0500

    Primer commit

---
+++ src/main/java/com/microservice/microservicioreactivo/MicroservicioreactivoApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.microservicioreactivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioreactivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioreactivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/microservicioreactivo/MicroservicioreactivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.microservicioreactivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioreactivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
